022604c2679fcfc618e2a34fbdedb884cf987d1f
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2020-07-20 18:41:30 +0900

    Updates

---
+++ precache-manifest.45c55e2cf37d0420d2389312cf97a905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fff68905266ec820ff92440a9083f4",
+    "url": "/react-app-renshu/index.html"
+  },
+  {
+    "revision": "9b90d16f9e83870aaa89",
+    "url": "/react-app-renshu/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/react-app-renshu/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-renshu/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b90d16f9e83870aaa89",
+    "url": "/react-app-renshu/static/js/main.33a430d4.chunk.js"
+  },
+  {
+    "revision": "15fe8274d1dfc0c9583d",
+    "url": "/react-app-renshu/static/js/runtime-main.cd6476a8.js"
+  }
+]);
